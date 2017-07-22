93ab5302291436566f7e76a771e64fba3224edd0
Andrew Greiner <andrewgreiner95@gmail.com>
2017-07-22 14:33:22 -0500

    Adds FAQ questions

--- src/FAQ/FAQ.js
+++ src/FAQ/FAQ.js
@@ -10,15 +10,26 @@ class FAQ extends Component {
       <div className="FAQ">
         <div className="Question">
           <Card>
-              <CardHeader
-                title="7-Day Carloadings vs Daily Carloadings"
-                actAsExpander={true}
-                showExpandableButton={true}/>
-              <CardText expandable={true}>
-                Answer coming soon...
-              </CardText>
-            </Card>
-          </div>
+            <CardHeader
+              title="7-Day Carloadings vs Daily Carloadings?"
+              actAsExpander={true}
+              showExpandableButton={true}/>
+            <CardText expandable={true}>
+              7-Day Carloadings are the averages of the Daily Carloadings over the course of the previous seven days. More specifically they are rolling averages.
+            </CardText>
+          </Card>
+        </div>
+        <div className="Question">
+          <Card>
+            <CardHeader
+              title="Will we be able to use data from 2015?"
+              actAsExpander={true}
+              showExpandableButton={true}/>
+            <CardText expandable={true}>
+              No, the data from 2015 is purposefully withheld in order to more accurately score your solution. Your model is intended to be able to generalize well enough to be able to predict any date given within a resonable time frame.
+            </CardText>
+          </Card>
+        </div>
       </div>
     );
   }
