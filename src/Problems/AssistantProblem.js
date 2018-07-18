ebe868ef86a0fb9036da6bdb4ba0f9a2dc0647f9
Renee Dennis <reneenicoledennis@gmail.com>
2018-07-17 21:48:53 -0500

    chore: Add more info to assistant problem

--- src/Problems/AssistantProblem.js
+++ src/Problems/AssistantProblem.js
@@ -4,13 +4,35 @@ import lifecycle from './lifecycle.png';
 import conversation from './conversation.png';
 
 const mockEquipment = {
-  id: 'MTKG45321',
+  id: 'SHMC1234',
   empty: false,
-  commodity: '',
+  commodity: 'Beer',
   carType: '',
   completedEvents: [],
   scheduledEvents: [],
   serviceIssues: []
+};
+
+const mockServiceIssue = {
+  dateOpened: '',
+  dateLastUpdated: '',
+  reason: '',
+  status: '',
+  equipmentId: '',
+  referenceNumber: '',
+  comments: [
+    {
+      dateCreated: '',
+      author: '',
+      company: '',
+      comments: ''
+    }
+  ]
+};
+
+const mockNotification = {
+  type: '',
+  dateTime: ''
 };
 
 export class AssistantProblem extends Component {
@@ -21,25 +43,29 @@ export class AssistantProblem extends Component {
         <p>
           Today, our customers have to navigate nearly 60 different applications
           to complete their daily tasks. As the customer's experience is
-          becoming a higher priority, we're starting to explore our options for
-          how to improve how every customer interacts with our services and
-          systems.
+          becoming an even higher priority at Union Pacific, we're starting to
+          explore our options for how to improve how every one of our customers
+          interact with our services and systems.
         </p>
         <p>
-          That's where the customer-facing assistant comes into play; it's one
-          of many avenues we're exploring to improve how we interact with our
-          customers. We'd like you to take a stab at creating some form of an
-          intelligent assistant, whether it be text-based, voice-based or both,
-          that could help make working with us as seamless as possible.
+          Imagine this very real scenario: one of our customers' warehouses is
+          heavily refrigerated to preserve the quality of the goods they ship
+          with us. Their employees (by extension, our customers as well) use our
+          tracking and shipment management applications to monitor and handle
+          shipments with us. With their environment forcing them to don down
+          parkas and gloves, it's impossible to effectively navigate our systems
+          without removing their gloves. This particular customer was only able
+          to track one or two cars at a time, simply because they couldn't keep
+          their gloves off for long enough to type any more. What if they didn't
+          have to take off their gloves at all?
         </p>
-
         <p>
-          Benefits are more obvious with the smaller customers that don't ship
-          an immense amount of things with us, or that don't have large
-          technology departments, or even something as specific as customers who
-          work all day in heavily refrigerated warehouses that can't take off
-          their gloves for long periods of time to navigate our site on a
-          computer.
+          That scenario, and several others, are where the customer-facing
+          assistant comes into play; it's one of many avenues we're exploring to
+          improve how we interact with our customers. We'd like you to take a
+          stab at creating some form of an intelligent assistant, whether it be
+          text-based, voice-based or both, that could help make working with us
+          as seamless as possible.
         </p>
         <p className="ImageWrapper">
           <img
@@ -57,7 +83,7 @@ export class AssistantProblem extends Component {
           submit an inquiry about prices, and they certainly don't want to wait
           weeks (yes, with the current process, it can take weeks!) to recieve
           that price quote. They want a simplified interface, free from as much
-          internal rail jargon as possible.
+          of our internal rail jargon as possible.
         </p>
         <p>
           Every customer should be associated with one or more locations (often
@@ -68,12 +94,14 @@ export class AssistantProblem extends Component {
           Battle Creek, Michigan and Kellogg's in Omaha, Nebraska).
         </p>
         <p>
-          Customers often want visibility into their inventories for one or more
-          locations, typically for cars en route (either inbound to them or
-          outbound from them), cars at one of our serving areas, and cars
-          currently at their industry. A graphic reprenstation of this
-          "lifecycle" is shown below (albeit a little simplified for the sake of
-          this event).
+          Customers need as much visibility into their shipments as possible.
+          Their shipments are often split up into different "inventory" states
+          for one or more locations. Currently, customers can view their
+          shipments on a car (otherwise known as equipment) level. Each car is
+          typically referred as en route (either inbound to their industry yard
+          or outbound from their industry yard), at one of our serving yards, or
+          currently at their industry yard. A simplified graphic representation
+          of this "lifecycle" is shown below.
         </p>
         <p className="ImageWrapper">
           <img
@@ -93,15 +121,34 @@ export class AssistantProblem extends Component {
         </p>
         <div className="ContentHeader">Data</div>
         <p>
-          We don't have specific data sets for this problem. However, we do want
-          to give you some examples of common information associated with
-          cars/equipment, exceptions, and service issues.
+          We don't have specific data sets for this problem. We don't want to
+          restrict you to our (sometimes overly) complex data. As most of you
+          are probably still rail newbies, you provide a valuable perspective on
+          how to approach this particular problem. We hope that you can think
+          outside of the box, extrapolate, and derive some fresh ideas. However,
+          we do want to give you some examples of critical and common
+          information associated with cars/equipment, exceptions, service
+          issues, etc.
         </p>
-        <div className="CodeBlock">
-          <div className="CodeBlockTitle">Mock Equipment</div>
-          <pre className="CodeBlockCode">
-            {JSON.stringify(mockEquipment, null, 2)}
-          </pre>
+        <div className="FlexContainer">
+          <div className="CodeBlock">
+            <div className="CodeBlockTitle">Mock Car/Equipment</div>
+            <pre className="CodeBlockCode">
+              {JSON.stringify(mockEquipment, null, 2)}
+            </pre>
+          </div>
+          <div className="CodeBlock">
+            <div className="CodeBlockTitle">Mock Service Issue</div>
+            <pre className="CodeBlockCode">
+              {JSON.stringify(mockServiceIssue, null, 2)}
+            </pre>
+          </div>
+          <div className="CodeBlock">
+            <div className="CodeBlockTitle">Mock Notification</div>
+            <pre className="CodeBlockCode">
+              {JSON.stringify(mockNotification, null, 2)}
+            </pre>
+          </div>
         </div>
       </div>
     );
