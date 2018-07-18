d2787e1a911d77abbb9a6ddeb3c2905a1a3be834
Renee Dennis <reneenicoledennis@gmail.com>
2018-07-18 17:59:00 -0500

    Add schedule

--- src/Schedule/Schedule.js
+++ src/Schedule/Schedule.js
@@ -8,8 +8,68 @@ export default class Schedule extends Component {
   render() {
     return (
       <div className="Schedule">
-        <Paper>
-          <p>hello world</p>
+        <Paper className="ScheduleContent">
+          <div className="ScheduleRow">
+            <div>
+              9:00 <small>AM</small>
+              - 10:00 <small>AM</small>
+            </div>
+            <div>Arrive, get your t-shirt, and set up</div>
+          </div>
+          <div className="ScheduleRow">
+            <div>
+              10:00 <small>AM</small> - 10:30 <small>AM</small>
+            </div>
+            <div>Problem overview and Q&A session</div>
+          </div>
+          <div className="ScheduleRow">
+            <div>
+              10:30 <small>AM</small>
+            </div>
+            <div>Start hacking!</div>
+          </div>
+          <div className="ScheduleRow">
+            <div>
+              12:00 <small>PM</small>
+            </div>
+            <div>Lunch</div>
+          </div>
+          <div className="ScheduleRow">
+            <div>
+              6:00 <small>PM</small>
+            </div>
+            <div>Dinner</div>
+          </div>
+          <div className="ScheduleRow">
+            <div>
+              12:00 <small>AM</small>
+            </div>
+            <div>Ice cream and snacks</div>
+          </div>
+          <div className="ScheduleRow">
+            <div>
+              7:00 <small>AM</small>
+            </div>
+            <div>Breakfast</div>
+          </div>
+          <div className="ScheduleRow">
+            <div>
+              8:30 <small>AM</small>
+            </div>
+            <div>Commit all of your code!</div>
+          </div>
+          <div className="ScheduleRow">
+            <div>
+              8:40 <small>AM</small> - 9:40 <small>AM</small>
+            </div>
+            <div>Presentations</div>
+          </div>
+          <div className="ScheduleRow">
+            <div>
+              10:00 <small>AM</small>
+            </div>
+            <div>Announce winners</div>
+          </div>
         </Paper>
       </div>
     );
