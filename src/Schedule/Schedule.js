40ec7c64cc1e53f329f1a7c7199fc5b28ba4c978
Andrew Greiner <andrewgreiner95@gmail.com>
2018-07-20 23:38:15 -0500

    Add memes af

--- src/Schedule/Schedule.js
+++ src/Schedule/Schedule.js
@@ -5,12 +5,32 @@ import { Paper } from '@material-ui/core';
 import cvmeme from '../Memes/cvmeme.jpg';
 import taxmeme from '../Memes/taxmeme.png';
 import kirbzmeme from '../Memes/kirbzmeme.jpg';
+import dronememe from '../Memes/dronememe.jpg';
+import fastmlmeme from '../Memes/fastmlmeme.jpg';
+import pmmeme from '../Memes/pmmeme.jpg';
+import reactmeme from '../Memes/reactmeme.jpg';
+import httpmeme from '../Memes/httpmeme.jpg';
+import brainmeme from '../Memes/brainmeme.jpg';
+import dbmeme from '../Memes/dbmeme.jpg';
+import dumbmlmeme from '../Memes/dumbmlmeme.png';
+import stackoverflowmeme from '../Memes/stackoverflowmeme.png';
+import windowsmeme from '../Memes/windowsmeme.jpg';
+import fpsmeme from '../Memes/fpsmeme.jpg';
+import blockchainmeme from '../Memes/blockchainmeme.png';
+import metalearningmeme from '../Memes/metalearningmeme.jpg';
+import binarypantsmeme from '../Memes/binarypantsmeme.jpg';
+import sourcecontrolmeme from '../Memes/sourcecontrolmeme.png';
+import nvidiameme from '../Memes/nvidiameme.png';
 
 import './Schedule.css';
 
 export default class Schedule extends Component {
   getRandomMeme() {
-    const memes = [cvmeme];
+    const memes = [
+      cvmeme, taxmeme, kirbzmeme, dronememe, fastmlmeme, pmmeme, reactmeme, httpmeme,
+      brainmeme, dbmeme, dumbmlmeme, stackoverflowmeme, windowsmeme, fpsmeme, blockchainmeme,
+      metalearningmeme, binarypantsmeme, sourcecontrolmeme, nvidiameme
+    ];
     return memes[Math.floor(Math.random() * memes.length)];
   }
   render() {
